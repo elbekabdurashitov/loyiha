997e73eb50fd679bbfcacee408bc9178598e9ed0
Elbek <elbekjon123@gmail.com>
2022-09-10 17:40:12 +0500

    routerni ornatdik

---
+++ src/components/NavBar.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="bg-sky-300 p-4 flex justify-center lg:justify-start gap-2">
+        <router-link v-for="yollanma in yollanmalar" :key="yollanma" class="bg-sky-500 text-white py-1 px-4 rounded-full font-bold border border-white hover hover:bg-white hover:text-sky-500 duration-300 hover:border-sky-500" :to="yollanma.manzil">{{yollanma.nom}}</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "NavBar",
+    data() {
+        return {
+            yollanmalar: [
+                {
+                    manzil: '/',
+                    nom: 'Bosh sahifa',
+                },
+                {
+                    manzil: '/about',
+                    nom: 'Haqida',
+                }
+            ]
+
+        }
+    }
+
+}
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1 class="text-4xl lg:text-6xl text-slate-900 font-bold py-8 px-12 lg:px-48 lg:mt-16 text-center">Rapidly build modern websites without ever leaving your HTML.</h1>
+    <p class="text-center text-xl text-gray-600 px-4 lg:px-64">
+      A utility-first CSS framework packed with classes like 
+      <span class="text-sky-500 font-mono"> flex, </span> 
+      <span class="text-sky-500 font-mono">pt-4,</span> 
+      <span class="text-sky-500 font-mono">text-center</span> 
+      and 
+      <span class="text-sky-500 font-mono">rotate-90</span> 
+      that can be composed to build any design, directly in your markup.
+    </p>
+    <div class="p-4 lg:px-64">
+      <button class="w-full bg-slate-900 text-white py-3 rounded-lg font-bold">Get started</button>
+    </div>
+  </div>
+</template>
